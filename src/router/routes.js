1662234d96af9af260876a59825fdcdf1bfbd9cf
tomatopickles404 <tomatopickles404@gmail.com>
2025-07-11 17:27:58 +0900

    refactor: 404 페이지 경로 추가

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,6 @@
 import { ProductListPage, initializeProductListPage } from "../pages/ProductListPage";
 import { ProductDetailPage, initializeProductDetailPage } from "../pages/ProductDetailPage";
+import { NotFoundPage } from "../pages/NotFoundPage";
 
 export const routes = [
   {
@@ -11,5 +12,9 @@ export const routes = [
     path: "/product/:productId",
     component: ProductDetailPage,
     initializer: initializeProductDetailPage,
+  },
+  {
+    path: "/non-existent-page",
+    component: NotFoundPage,
   },
 ];
